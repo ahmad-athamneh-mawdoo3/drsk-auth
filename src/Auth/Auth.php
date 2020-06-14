f12656b538ebb2d3d1223992b7c708c1d34a2cf9
ahmad athamneh <ahmad.athamneh@mawdoo3.com>
2020-06-14 03:55:29 +0300

    change package name

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -4,7 +4,7 @@ namespace Mawdoo3\Drsk\Auth;
 
 class Auth  
 {
-    public static function isAuthed($user = null)
+    public function isAuthed($user = null)
     {
         return isset($user) && !is_null($user) ? "$user logged in from auth" : 'not logged from auth';
     }
